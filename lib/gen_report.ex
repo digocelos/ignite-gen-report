de818384339318a22b3dbf2474f655f574986161
rodrigo.vasconcelos <rodrigo.vasconcelos@siagri.com.br>
2021-05-14 17:55:06 -0300

    Adicionado método para calcular o total de horas

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,9 +1,56 @@
 defmodule GenReport do
+  @moduledoc """
+    Generate reports
+
+    Ten people freelaxed for a company X for five years and the history with all the data
+    of each one of these people (name, hours worked, day, month and year) were transferred
+    to a CSV file in the following order: name, hours the day (which will vary from 1 to 8 hours),
+    day (which will vary from 1 to 30 even for the month of February and without considering leap years)
+    referring to the hours of work, month and year (which goes from 2016 to 2020 ).
+    In short: ** name **, ** number of hours **, ** day **, ** month ** and ** year **.
+
+    Provides function build/1 to reade and convert file in List
+  """
+
   alias GenReport.Parser
 
+  @avaliable_member [
+    "Daniele",
+    "Mayk",
+    "Giuliano",
+    "Cleiton",
+    "Jakeliny",
+    "Joseph",
+    "Danilo",
+    "Diego",
+    "Cleiton",
+    "Rafael",
+    "Vinicius"
+  ]
+
+  @doc """
+  Generate report
+
+  ## Parameters
+    - fileName : String of a file where read
+  """
   def build(fileName) do
     fileName
     |> Parser.parse_file()
-    |> Enum.each(fn line -> IO.puts(line) end)
+    |> Enum.reduce(report_acc(), fn line, report -> report_all_hours(line, report) end)
   end
+
+  defp report_all_hours([name, hours, _month, _day, _year], %{"all_hours" => all_hours}) do
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+
+    build_report(all_hours)
+  end
+
+  def report_acc() do
+    all_hours = Enum.into(@avaliable_member, %{}, &{&1, 0})
+
+    build_report(all_hours)
+  end
+
+  defp build_report(all_hours), do: %{"all_hours" => all_hours}
 end
--- lib/parser.ex
+++ lib/parser.ex
@@ -1,13 +1,32 @@
 defmodule GenReport.Parser do
+  @moduledoc """
+  Provides function parse_file/1 to reade and convert file in List
+  """
+
+  @doc """
+  Read and return a List with all lines of a CSV file.
+
+  ## Parameters
+
+    - fileName: String that representas the file to read and parser.
+  """
   def parse_file(fileName) do
     "report/#{fileName}"
     |> File.stream!()
-    |> Stream.map(&{parse_line(&1)})
+    |> Stream.map(fn line -> parse_line(line) end)
   end
 
+  @doc """
+  Parser a line, removing spaces and split in array
+
+  ## Parameters
+
+    - line: String that represents the line read of the file.
+  """
   defp parse_line(line) do
     line
     |> String.trim()
     |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
   end
 end
